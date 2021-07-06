a2c1d5a312015cbae1bac4d1be50e2a737edcfe3
HULKANNAKLUHAR <rahulkanna466@gmail.com>
2021-07-06 13:31:08 +0530

    added balance dashboard

--- app.js
+++ app.js
@@ -1,41 +1,62 @@
 //selectors
+var cashInHand = 0
 const textBox1 = document.querySelector("#add")
 const textBox2 = document.querySelector("#sub")
 const button1 = document.querySelector("#btnadd")
 const button2 = document.querySelector("#btnsub")
 const histryDiv = document.querySelector(".entryList")
+const cashbal = document.querySelector("#cinbalance")
 
 //event listerners
 button1.addEventListener('click', genlist)
+button2.addEventListener('click', genlist)
 
 //functions
 function genlist(event) {
     event.preventDefault()
-    if (event.target.innerText === 'add') {
+    if (event.target.innerText == 'add') {
         const boxvalue = parseInt(textBox1.value)
         if (boxvalue) {
-            const entrycontainer = document.createElement('div')
-            entrycontainer.classList.add("entrys")
+            // const entrycontainer = document.createElement('div')
+            // entrycontainer.classList.add("entrys")
             const listItem = document.createElement('li')
             listItem.innerText = boxvalue
-            listItem.classList.add('list-item')
-            entrycontainer.appendChild(listItem)
-            histryDiv.appendChild(entrycontainer)
+            listItem.classList.add('list-item-income')
+                // entrycontainer.appendChild(listItem)
+                // histryDiv.appendChild(entrycontainer)
+            histryDiv.appendChild(listItem)
+            cashInHand += boxvalue
+            cashbal.innerText = cashInHand
+            saveToLocal(boxvalue)
+
 
         }
     } else {
         const boxvalue = parseInt(textBox2.value)
         if (boxvalue) {
-            const entrycontainer = document.createElement('div')
-            entrycontainer.classList.add("entrys")
+            // const entrycontainer = document.createElement('div')
+            // entrycontainer.classList.add("entrys")
             const listItem = document.createElement('li')
             listItem.innerText = boxvalue
-            listItem.classList.add('list-item')
+            listItem.classList.add('list-item-expense')
             entrycontainer.appendChild(listItem)
-            histryDiv.appendChild(entrycontainer)
+            histryDiv.appendChild(listItem)
+            cashInHand -= boxvalue
+            cashbal.innerText = cashInHand
+            saveToLocal(boxvalue)
         }
+    }
+}
+localStroage.
 
 
+function saveToLocal(boxvalue) {
+    let entryArray
+    if () {
 
+        entryArray = []
+
+    } else {
 
     }
+}
